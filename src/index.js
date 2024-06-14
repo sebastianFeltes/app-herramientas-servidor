959cd33cefa34b239efcd2bdd097a028c85aaa6d
sebastianFeltes <jikansui16@gmail.com>
2024-06-14 20:31:48 -0300

    ready to start coding

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const port = 4000;
 //SETTEO DEL SERVIDOR PARA CORS, PARSEOS Y ENCRIPTACION
 app.use(cors());
 app.use(json());
-app.use(urlencoded())
+app.use(urlencoded());
 
+//IMPORTAR RUTAS
 app.listen(port, () => {
   console.log("Servidor escuchando en el puerto 4000");
 });
